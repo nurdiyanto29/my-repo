8d89e42ba1fa153e42b10ebd7be4f866ad314a7e
nurdiyanto29 <nurdi014@gmail.com>
2022-12-28 21:56:18 +0700

    uuu

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var audio = new Audio('assets/sentmessage.mp3');
-var contactString = "<div class='social'> <a target='_blank' href='tel:+918600765857'> <div class='socialItem' id='call'><img class='socialItemI' src='images/phone.svg'/><label class='number'>8600765857</label></div> </a> <a href='mailto:patilvinu777@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/Vinayak-09'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> <a target='_blank' href='https://wa.me/918600765857'> <div class='socialItem'><img class='socialItemI' src='images/whatsapp.svg' alt=''></div> </a> <a target='_blank' href='https://t.me/vinayak_09'> <div class='socialItem'><img class='socialItemI' src='images/telegram.svg' alt=''></div> </a> <a target='_blank' href='https://instagram.com/vinayak_patil_09'> <div class='socialItem'><img class='socialItemI' src='images/instagram.svg' alt=''> </div> </a> <a href='https://www.linkedin.com/in/vinayak-patil-793bb5206/' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
+var contactString = "<div class='social'><a href='mailto:nurdi014@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/nurdiyanto29'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> <a target='_blank' href='https://t.me/nrdynt29'> <div class='socialItem'><img class='socialItemI' src='images/telegram.svg' alt=''></div> </a> <a target='_blank' href='https://instagram.com/nrdynt29'> <div class='socialItem'><img class='socialItemI' src='images/instagram.svg' alt=''> </div> </a> <a href='https://www.linkedin.com/in/nurdiyanto-9044b2242/' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
 var resumeString = "<img src='images/resumeThumbnail.png' class='resumeThumbnail'><div class='downloadSpace'><div class='pdfname'><img src='images/pdf.png'><label>Vinayak Resume.pdf</label></div><a href='assets/Vinayak Patil&#39;s Resume.pdf' download='Vinayak_Patil_Resume.pdf'><img class='download' src='images/downloadIcon.svg'></a></div>";
-var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238.63833262443757!2d74.19014864534314!3d16.865338763272877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1a7dcf40f5dd7%3A0xd7b69fe1fcfa9877!2zMTbCsDUxJzU1LjQiTiA3NMKwMTEnMjUuMyJF!5e0!3m2!1sen!2sin!4v1645079906766!5m2!1sen!2sin' class='map'></iframe></div><label class='add'><address>B2 'Asara'<br>Kodoli<br>Kolhapur, Maharashtra, INDIA 416114</address>";
+var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56316.2895613665!2d110.40584827043645!3d-8.018930989410121!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7bac1dac27c327%3A0x24028babac05ef61!2sKec.%20Panggang%2C%20Kabupaten%20Gunung%20Kidul%2C%20Daerah%20Istimewa%20Yogyakarta!5e0!3m2!1sid!2sid!4v1672237137786!5m2!1sid!2sid' class='map'></iframe></div><label class='add'><address>Panggang<br>Gunungkidul, Yogyakarta</address>";
 
 function startFunction() {
     setLastSeen();
@@ -74,21 +74,21 @@ function waitAndResponce(inputText) {
     switch (inputText.toLowerCase().trim()) {
         case "intro":
             setTimeout(() => {
-                sendTextMessage2("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>Nurdiyanto</a>.</span><br><br>I am a Computer Engineering student at <span class='bold'>Ch. Shivaji University 👨🏻‍💻📚</span><br><br>I am eager to hear about potential career opportunities, so I would be pleased to chat about job openings in the engineering sphere.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
+                sendTextMessage2("Hello Semua 👋🏻,<br><br>Perkenalkan, nama saya <span class='bold'><a class='alink'>Nurdiyanto</a>.</span><br><br>Saya adalah seorang web developer yang masih dalam tahap belajar dan terus belajar 👨🏻‍💻📚<br><br>Saat ini saya bekerja di salah satu perusahan swasta di Yogyakarta sebagai <span class='bold'>FullStack Web Developer</span><br><br>Oh iya,  kalian bisa ketikan <span class='bold'>'help'</span> untuk tau tentang saya lebih detail.<br>");
             }, 2000);
             break;
         case "help":
-            sendTextMessage("<span class='sk'>Send Keyword to get what you want to know about me...<br>e.g<br><span class='bold'>'skills'</span> - to know my skills<br><span class='bold'>'resume'</span> - to get my resume<br><span class='bold'>'education'</span> - to get my education details<br><span class='bold'>'address'</span> - to get my address<br><span class='bold'>'contact'</span> - to get ways to connect with me<br><span class='bold'>'projects'</span> - to get details of my projects<br><span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span>");
+            sendTextMessage("<span class='sk'>Kirim Kata Kunci berikut untuk mendapatkan apa yang ingin Anda ketahui tentang saya... <br>misal<br><span class='bold'>'skills'</span> - untuk mengetahui skills saya<br><span class='bold'>'education'</span> - untuk melihat detail pendidikan saya<br><span class='bold'>'address'</span> - untuk mendapatkan alamat saya<br><span class='bold'>'contact'</span> - untuk melihat dan terhubung dengan saya<br><span class='bold'>'projects'</span> - untuk melihat detail projek yang pernah saya kerjakan<br><span class='bold'>'clear'</span> - untuk menghapus percakapan ini<br><span class='bold'>'about'</span> - untuk tau tentang portofolio ini</span>");
             break;
-        case "resume":
-            sendTextMessage(resumeString);
-            break;
+            // case "resume":
+            //     sendTextMessage(resumeString);
+            //     break;
         case "skills":
-            sendTextMessage("<span class='sk'>I am currently pursuing B.Tech degree in Computer Science Engineering.<br><br>I can comfortably write code in following languages :<br><span class='bold'>Java<br>C++<br>C<br>PHP<br>Kotlin<br>Dart<br>Python<br>CSS<br>HTML</span><br><br>I've experiance with following frameworks :<span class='bold'><br>Android<br>Flutter<br>ReactJs<br>GTK</span><br><br>I use <span class='bold'>Arch Linux</span> as daily driver on my HP Pavilion 15-ec0xxx<br>OS:Arch Linux<br>DE:Gnome(More often) Kde(often)<br>Favourite IDE:VSCode</span>");
+            sendTextMessage("<span class='sk'>Berikut ini beberapa skills dan bahasa pemrograman yang saya kuasai:<br><span class='bold'>PHP<br>NODE JS<br>HTML<br>TWIG<br>Laravel<br>CSS<br>Javascript<br>API<br>Bootstrap<br>MySql<br>Postgresql</span>");
             break;
 
         case "education":
-            sendTextMessage("I am currently pusuing B.Tech degree in Computer Science Engineering from TKIET Kolhapur<br>Passing Year : 2023<br><br>I have completed my Diploma from Government Polytechnic Karad<br>Passing Year:2020<br>Result:86.06%<br><br>I have completed my Secondary school from local school known as SWV<br>Passing Year:2016");
+            sendTextMessage("Saya telah menyelesaikan kuliah saya dari Universitas Teknologi Digital Indonesia Tahun Lulus:2022 dengan Gelar Sarjana Komputer <br> <br> Saya telah menyelesaikan sekolah menengah saya dari SMA N 1 Panggang pada tahun 2017");
             break;
 
         case "address":
@@ -98,20 +98,20 @@ function waitAndResponce(inputText) {
             clearChat();
             break;
         case "about":
-            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript from SCRATCH!<br><br>👨🏻‍💻 Designed and Developed by <a class='alink' target='_blank' href='https:\/\/instagram.com/vinayak_patil_09/'><span class='bold'>Vinayak Patil</a> with ❤️</span>");
+            sendTextMessage("🛠️💻 Portofolio ini saya buat menggunakan HTML, CSS dan JavaScript dengan penuh ❤️");
             break;
         case "contact":
             sendTextMessage(contactString);
             break;
         case "projects":
-            sendTextMessage("You want to check my projects? Then just jump into my Github Account.<br><br><div class='social'><a target='_blank' href='https://github.com/Vinayak-09'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a></div>");
+            sendTextMessage("<span class='sk'>Berikut adalah beberapa Project yang pernah saya kerjakan:<br><span class='bold2'>- Routelink<br>Multi Kontrol Nusantara<br>- Trans Security<br>- IBM Incubator<br>- Sistem Keuangan Masjid<br>- GKI Adisucipto<br>- Rumaysho Store<br>- SIG Merapi<br>- Sistem Sewa Barongsai<br>- Trigana Air<br>- Web service PKK</span> <br><br>Jika kalian ingin tahu tentang projek saya? kalian bisa lihat di repositori github saya<br><br><div class='social'><a target='_blank' href='https://github.com/nurdiyanto29'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a></div>");
             break;
         case "new":
             sendTextMessage(addressString);
             break;
         default:
             setTimeout(() => {
-                sendTextMessage("Hey I couldn't catch you...😢<br>Send 'help' to know more about usage.");
+                sendTextMessage("Maaf saya tidak dapat membaca kata kunci yang kamu ketikkan...😢<br>Ketik dan kirim 'help' untuk tau bagaimana menggunakannya ");
             }, 2000);
             break;
     }
